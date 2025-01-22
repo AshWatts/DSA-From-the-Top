211436cd1174e12b4b61e5a9f2fa08c855e9e808
Ayush W <113709655+AshWatts@users.noreply.github.com>
2025-01-22 23:59:22 +0530

    `

---
+++ Linked List/Basic_Syntax.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+    public:
+    List() {
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newnode = new Node(val);
+        if(head == NULL) {
+            head = tail = newnode;
+            return;
+        }
+        else { 
+            newnode->next = head;                           // (*newnode).next = head;
+            head = newnode;
+        }
+    }
+
+    void push_back(int val) {
+        Node* newnode = new Node(val);
+        if(head == NULL) {
+            head = tail = newnode;
+            return;
+        }
+        else {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+    void pop_front() {
+        if(head == NULL) {
+            cout<<"Already Empty Linked List!";
+            return;
+        }
+
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+
+        delete temp;
+    } 
+
+    void pop_back() {
+        if(head == NULL) {
+            cout<<"Already Empty Linked List!";
+            return;
+        }
+
+        Node* temp = head;
+        while(temp->next != tail) {                 // while(temp->next->next != NULL)
+            temp = temp->next;
+        }
+
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+    }
+
+    void show() {
+        Node* temp = head;
+        while(temp != NULL) {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+
+};
+
+int main() {
+    List ll;
+
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.push_front(120);
+
+    ll.show();
+
+    ll.push_back(3);
+    ll.push_back(745);
+
+    ll.show();
+
+    ll.pop_front();
+
+    ll.show();
+
+    ll.pop_back();
+
+    ll.show();
+
+    return 0;
+}
