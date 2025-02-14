cbfdac8ee772cff55dfc37239dca9dcd47d52674
Ayush Wattamwar <ayushwattamwar27@gmail.com>
2025-02-14 15:44:37 +0530

    `

---
+++ Stacks/Previous_Smaller_Element.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {1, 3, 2, 4}; 
+
+    // Previous Smaller Element
+
+    stack<int> s;
+    vector<int> ans(arr.size(), 0);
+
+    for(int i=0; i<arr.size(); i++) {
+        while(s.size() > 0 && s.top() >= arr[i]) {
+            s.pop();
+        }
+        if(s.empty()) {
+            ans[i] = -1;
+        }
+        else {
+            ans[i] = s.top();
+        }
+        s.push(arr[i]);
+    }
+
+    for(int i=0; i<arr.size(); i++) {
+        cout<<arr[i]<<" -> "<<ans[i]<<endl;
+    }
+
+    return 0;
+}
