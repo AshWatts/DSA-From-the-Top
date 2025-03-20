42324bed2ad2aeb70aa67c7c3bee99fe94af3230
Ayush Wattamwar <ayushwattamwar27@gmail.com>
2025-03-20 15:18:02 +0530

    `

---
+++ Queue/Implementation_using_LL.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class Queue {
+    Node* head;
+    Node* tail;
+public:
+    Queue() {
+        head = tail = NULL;
+    }
+
+    void push(int data) {
+        Node* newNode = new Node(data);
+        if(isempty()) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop() {
+        if(isempty()) {
+            cout<<"Cannot pop, Queue is empty!";
+            return;
+        }
+        else {
+            Node* temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    int front() {
+        if(isempty()) {
+            cout<<"Queue is empty!";
+            return;
+        }
+        else {
+            
+        }
+    }
+
+    bool isempty() {
+        return head == NULL;
+    }
+};
+
+int main() {
+    
+}
